6c8fa5d6ca8658949a0012f8c5fe141b0036d03e
Leonel <leonelaf12@gmail.com>
2019-09-20 11:49:59 -0400

    finished ring buffer and got names.py to about .9 seconds

--- names/names.py
+++ names/names.py
@@ -10,13 +10,28 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_2.sort()
+names_1.sort()
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+
+def binary_search(names, name):
+    middle = len(names) // 2
+    while len(names) > 1:
+        if name == names[middle]:
+            return duplicates.append(name)
+        elif name < names[middle]:
+            return binary_search(names[:middle], name)
+        elif name > names[middle]:
+            return binary_search(names[middle:], name)
+    return duplicates
+
+
+#duplicates = [x for x in names_1 if x in names_2]
+for n in names_1:
+    binary_search(names_2, n)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
